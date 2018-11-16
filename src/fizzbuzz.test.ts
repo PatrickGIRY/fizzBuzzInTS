b61b4f223695ebbbab02c8d4d470fb7ef5fac511
Patrick GIRY <patrickgiry@gmail.com>
2018-11-16 20:11:40 +0100

    Refactoring linter corrections

--- src/fizzbuzz.test.ts
+++ src/fizzbuzz.test.ts
@@ -1,22 +1,22 @@
-import { printIt } from '.'
+import { printIt } from '.';
 
 describe('Fizzbuzz should', () => {
   test('print a string 1 when given an integer 1', () => {
-    expect(printIt(1)).toEqual("1");
+    expect(printIt(1)).toEqual('1');
   });
   test('print a string 2 when given an integer 2', () => {
-    expect(printIt(2)).toEqual("2");
+    expect(printIt(2)).toEqual('2');
   });
   test('print a string Fizz when given an integer 3', () => {
-    expect(printIt(3)).toEqual("Fizz");
+    expect(printIt(3)).toEqual('Fizz');
   });
   test('print a string Fizz when given an integer 6', () => {
-    expect(printIt(6)).toEqual("Fizz");
+    expect(printIt(6)).toEqual('Fizz');
   });
   test('print a string Buzz when given an integer 5', () => {
-    expect(printIt(5)).toEqual("Buzz");
+    expect(printIt(5)).toEqual('Buzz');
   });
   test('print a string FizzBuzz when given an integer 15', () => {
-    expect(printIt(15)).toEqual("FizzBuzz");
+    expect(printIt(15)).toEqual('FizzBuzz');
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 
-let multipleOf: (m:number) => (n:number) => boolean =
-  (m => n => n % m == 0);
+const multipleOf: (m:number) => (n:number) => boolean =
+  (m => n => n % m === 0);
 
-let rules: {predicate :(n:number) => boolean ,mapping: string}[] = [
-  {predicate: multipleOf(3), mapping: "Fizz"}
-, {predicate: multipleOf(5), mapping: "Buzz"}
+const rules: { predicate :(n:number) => boolean, mapping: string }[] = [
+  { predicate: multipleOf(3), mapping: 'Fizz' },
+  { predicate: multipleOf(5), mapping: 'Buzz' },
 ];
 
 export function printIt(n:number):string {
-  let result:string = rules
-          .filter(({predicate}) => predicate(n))
-          .map(({mapping}) => mapping)
+  const result:string = rules
+          .filter(({ predicate }) => predicate(n))
+          .map(({ mapping }) => mapping)
           .join('');
-  return result != "" ? result : n.toString();
+  return result !== '' ? result : n.toString();
 }
