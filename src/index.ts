7894a2199eaf32e9732faaa7d9576002ea27a21f
Patrick GIRY <patrickgiry@gmail.com>
2018-11-16 17:17:44 +0100

    Refactoring in functional way

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 
+let multipleOf: (m:number) => (n:number) => boolean =
+  (m => n => n % m == 0);
+
+let rules: [(n:number) => boolean ,string][] = [
+  [multipleOf(3), "Fizz"]
+, [multipleOf(5), "Buzz"]
+];
+
 export function printIt(n:number):string {
-  let result:string = "";
-  if(n % 3 == 0)
-    result += "Fizz";
-  if(n % 5 == 0)
-    result += "Buzz";
+  let result:string = rules
+          .filter(rule => rule[0](n))
+          .map(rule => rule[1])
+          .reduce((r, s) => r += s, "");
   return result != "" ? result : n.toString();
 }
